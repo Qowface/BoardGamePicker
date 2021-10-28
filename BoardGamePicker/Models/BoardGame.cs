64920d328e9360214d2c7c70b6bcf04b50d489ba
Robert Marquess <qowsrule@gmail.com>
2021-10-28 19:42:41 -0400

    Add data annotations for field display names.

--- BoardGamePicker/Models/BoardGame.cs
+++ BoardGamePicker/Models/BoardGame.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,13 @@ namespace BoardGamePicker.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Minimum Players")]
         public int MinPlayers { get; set; }
+        [Display(Name = "Maximum Players")]
         public int MaxPlayers { get; set; }
+
+        [Display(Name = "Length (minutes)")]
         public int Length { get; set; }
     }
 }
