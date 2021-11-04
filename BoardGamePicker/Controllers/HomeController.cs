99a83634886cacda5b5367ccb3d741169c165ed1
Robert Marquess <qowsrule@gmail.com>
2021-11-04 19:47:03 -0400

    Add game search by players/time to main page.

--- BoardGamePicker/Controllers/HomeController.cs
+++ BoardGamePicker/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using BoardGamePicker.Models;
+﻿using BoardGamePicker.Data;
+using BoardGamePicker.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,15 +14,36 @@ namespace BoardGamePicker.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BoardGamePickerContext _bgContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BoardGamePickerContext bgContext)
         {
             _logger = logger;
+            _bgContext = bgContext;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Results(int players, int minutes)
+        {
+            var boardGames = from bg in _bgContext.BoardGame select bg;
+
+            if (players > 0)
+            {
+                boardGames = boardGames
+                    .Where(bg => players >= bg.MinPlayers)
+                    .Where(bg => players <= bg.MaxPlayers);
+            }
+
+            if (minutes > 0)
+            {
+                boardGames = boardGames.Where(bg => minutes >= bg.Length);
+            }
+
+            return View(await boardGames.ToListAsync());
         }
 
         public IActionResult Privacy()
